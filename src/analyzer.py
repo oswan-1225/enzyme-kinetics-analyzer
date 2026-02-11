57e69e8a012549bbc9cad7917cc86cfd73a02197
Owen Swanson <oswanson@uoregon.edu>
2026-02-11 11:40:56 -0800

    Made a preprocess and plot function. The preprocess function checks to see if the data is useable. The plot function is able to generate a scatter but Im still working out the fit parameters function that Im still defining in modelingeqs. I also added a test data file that has some substrate and velocity data that I can use to test the code.

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,1 +1,9 @@
-
+from utils import loaddata, plotdata, preprocess_data
+from modelingeqs import fitparam
+with open('data/testdata.csv') as data:
+    substrate, velocity = loaddata(data)
+    if preprocess_data(substrate, velocity):
+        print("Data preprocessing successful.")
+    else:
+        print("Data preprocessing failed. Please check the input data.")
+plotdata(substrate, velocity)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,27 @@
 import pandas as pd
+from modelingeqs import fitparam
+from numpy import linspace
 def loaddata(filename):
+    '''Load substrate and velocity data from a CSV file. The CSV file must contain 'substrate' and 'velocity' columns.'''
     df = pd.read_csv(filename)
     if 'substrate' not in df.columns or 'velocity' not in df.columns:
         raise ValueError("CSV file must contain 'substrate' and 'velocity' columns.")
-    return df['substrate'], df['velocity'] 
+    return df['substrate'], df['velocity']
+
+def preprocess_data(substrate, velocity: pd.Series):
+    '''checks the substrate and velocity data. This function checks for missing values and ensures that the data is of the same length.'''
+    if len(substrate) != len(velocity):
+        raise ValueError("Substrate and velocity data must be of the same length.")
+    return True
+
+def plotdata(substrate, velocity):
+    '''Plot substrate vs. velocity data using matplotlib.'''
+    import matplotlib.pyplot as plt
+    plt.scatter(substrate, velocity)
+    plt.xlabel('[S]')
+    plt.ylabel('Velocity')
+    plt.title('Substrate vs. Velocity')
+    x_fit = linspace(min(substrate), max(substrate), 100)
+    y_fit = fitparam(substrate, velocity)(x_fit)
+    plt.plot(x_fit, y_fit, label='Fitted Curve')
+    plt.show()
