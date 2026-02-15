763050e9d6e273f96f0ccfa3e6b480c30e3f5d49
Owen Swanson <oswanson@uoregon.edu>
2026-02-15 11:23:22 -0800

    Deleted bad function

--- src/modelingeqs.py
+++ src/modelingeqs.py
@@ -1,10 +1,2 @@
 def fitparam(substrate, velocity):
-    '''Fit the substrate and velocity data to the Michaelis-Menten equation to estimate Vmax and Km.'''
-    import numpy as np
-    from scipy import optimize
-    
-    def michaelis_menten(S, Vmax, Km):
-        return (Vmax * S) / (Km + S)
-    
-    popt, _ = optimize.curve_fit(michaelis_menten, substrate, velocity, bounds=(0, np.inf))
-    return popt  # Returns Vmax and Km
+    '''Fit the substrate and velocity data to the Michaelis-Menten equation to estimate Vmax and Km.''' 
