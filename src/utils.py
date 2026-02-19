351dfee21b2220236b50f5760e04c5aad20b6ec7
Owen Swanson <oswanson@uoregon.edu>
2026-02-18 17:23:29 -0800

    Added an R^2 feature to the utils and now appends R^2 to the results legend
    Current goals:
    Command line interface for running the model and generating results
    Concentrated output of results in a single file

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from modelingeqs import MMfit
 def loaddata(filename):
     '''Load substrate and velocity data from a CSV file. The CSV file must contain 'substrate' and 'velocity' columns.'''
@@ -13,20 +14,39 @@ def preprocess_data(substrate, velocity: pd.Series):
         raise ValueError("Substrate and velocity data must be of the same length.")
     return True
 
+def r2_score(y_true, y_pred):
+    '''Calculates R-squared of fit'''
+    y_true = np.asarray(y_true, dtype=float)
+    y_pred = np.asarray(y_pred, dtype=float)
+    ss_res = np.sum((y_true - y_pred) ** 2)
+    ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
+    return 1 - (ss_res / ss_tot) if ss_tot != 0 else 0.0
+
 def plotdata(substrate, velocity):
     '''Plot substrate vs. velocity data using matplotlib.'''
     import matplotlib.pyplot as plt
+
+    
+
     # Sets the axis constraints
     figure, ax = plt.subplots()
     ax.scatter(substrate, velocity, color="gold")
     ax.set_xlabel('[S]')
     ax.set_ylabel('Velocity')
     ax.set_title('Substrate vs. Velocity')
+
+
     # plots the eq using the MMfit function to fit the data and plot the resulting curve
     vmax, km = MMfit(substrate, velocity)
     xfit = range(int(min(substrate)), int(max(substrate)) + 1)
     yfit = [vmax * s / (km + s) for s in xfit]
-    ax.plot(xfit, yfit, color='pink', label=f'MM Fit: Vmax={vmax:.2f}, Km={km:.2f}')
+    r2 = r2_score(velocity, [vmax * s / (km + s) for s in substrate])
+    ax.plot(xfit, 
+    yfit, 
+    color='pink', 
+    label=f'MM Fit: Vmax={vmax:.2f}, Km={km:.2f}, R²={r2:.6f}'
+    )
+    ax.legend()
     return figure
 
 def save_figure(filename, fig=None):
